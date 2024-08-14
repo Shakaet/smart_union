9152386dabaa71e85452807a286d9d02a9b4bb03
Shakaet <abdshakaet@gmail.com>
2024-08-15 00:12:44 +0600

    add

--- app/src/main/java/com/example/smart_union/onnaonno.java
+++ app/src/main/java/com/example/smart_union/onnaonno.java
@@ -1,6 +1,10 @@
 package com.example.smart_union;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class onnaonno extends AppCompatActivity {
 
+    Button but3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_onnaonno);
+        but3=findViewById(R.id.but3);
+
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent ii=new Intent(getApplicationContext(), education.class);
+
+                Bundle translatebundle= ActivityOptions.makeCustomAnimation(getApplicationContext(),
+                        R.anim.slide_in_right,
+                        R.anim.slide_out_left).toBundle();
+
+
+
+                startActivity(ii,translatebundle);
+            }
+        });
 
     }
 }

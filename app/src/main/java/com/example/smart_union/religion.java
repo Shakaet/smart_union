8b1000ef07023c344ab6d577e946b98874771926
Shakaet <abdshakaet@gmail.com>
2024-08-15 17:34:04 +0600

    add

--- app/src/main/java/com/example/smart_union/religion.java
+++ app/src/main/java/com/example/smart_union/religion.java
@@ -12,23 +12,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class onnaonno extends AppCompatActivity {
+public class religion extends AppCompatActivity {
 
-    Button but3,but4;
+    Button but1,but2,but3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onnaonno);
+        setContentView(R.layout.activity_religion);
+        but1=findViewById(R.id.but1);
+        but2=findViewById(R.id.but2);
         but3=findViewById(R.id.but3);
-        but4=findViewById(R.id.but4);
 
-        but3.setOnClickListener(new View.OnClickListener() {
+        but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent ii=new Intent(getApplicationContext(), education.class);
+                Intent ii=new Intent(getApplicationContext(), mosque.class);
+
+                Bundle translatebundle= ActivityOptions.makeCustomAnimation(getApplicationContext(),
+                        R.anim.slide_in_left,
+                        R.anim.slide_out_right).toBundle();
+
+
+
+                startActivity(ii,translatebundle);
+            }
+        });
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent ii=new Intent(getApplicationContext(), temple.class);
 
                 Bundle translatebundle= ActivityOptions.makeCustomAnimation(getApplicationContext(),
                         R.anim.slide_in_right,
@@ -40,11 +57,11 @@ public class onnaonno extends AppCompatActivity {
             }
         });
 
-        but4.setOnClickListener(new View.OnClickListener() {
+        but3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent ii=new Intent(getApplicationContext(), religion.class);
+                Intent ii=new Intent(getApplicationContext(), buddho.class);
 
                 Bundle translatebundle= ActivityOptions.makeCustomAnimation(getApplicationContext(),
                         R.anim.slide_in_left,
@@ -55,6 +72,7 @@ public class onnaonno extends AppCompatActivity {
                 startActivity(ii,translatebundle);
             }
         });
+
 
     }
 }

05e1c26229744a60388b3b2717e85d339db536a3
Shakaet <abdshakaet@gmail.com>
2024-08-15 16:13:18 +0600

    add

--- app/src/main/java/com/example/smart_union/high_info.java
+++ app/src/main/java/com/example/smart_union/high_info.java
@@ -1,6 +1,7 @@
 package com.example.smart_union;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class high_info extends AppCompatActivity {
 
+    ListView l1;
+
+    String[] title={"গশ্চি উচ্চ বিদ্যালয়","পাচঁখাইন সম্মেলনী উচ্চ বিদ্যালয়","কোয়েপাড়া বালিকা উচ্চ বিদ্যালয়","কোয়েপাড়া উচ্চ বিদ্যালয়"};
+    String [] details={"স্থাপিত-১৯৮৬","স্থাপিত-১৯৯০","স্থাপিত-১৯৮৬","স্থাপিত-১৯৮৭"};
+
+    String[] info={"গ্রাম-গশ্চি,ওয়ার্ড-২ নং","গ্রাম-পাচঁখাইন,ওয়ার্ড-৪ নং","গ্রাম-কোয়েপাড়া,ওয়ার্ড-৮ নং","গ্রাম-কোয়েপাড়া,ওয়ার্ড-৭ নং"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_high_info);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        l1=findViewById(R.id.listview);
+
+        customAdapter2 adapter=new customAdapter2(high_info.this,title,details,info);
+
+        l1.setAdapter(adapter);
+
     }
 }
